62e1d682d4f2f51e8bbd16ddd60116f3b316832d
Drelofs <ali.shahid.1994@gmail.com>
2022-07-31 22:24:28 +0200

    Added styling for search page

--- resources/views/car/search.blade.php
+++ resources/views/car/search.blade.php
@@ -8,9 +8,9 @@
     <h1>Search results</h1>
     @if($cars->isNotEmpty())
         @foreach ($cars as $car)
-            <div class="car-list">
+            <div class="shadow-sm car-listitem bg-white py-2 px-2 my-2">
                 <p>{{ $car->name }} {{ $car->model}} ({{ $car->year }})</p>
-                
+                <a class="btn btn-dark float-right" href="/car/{{ $car->id }}">Show</a>            
             </div>
         @endforeach
     @else 
